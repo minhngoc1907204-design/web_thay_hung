7acc57d23a68313b65e680f84a3ad78224cbd296
ngoc <ngoc@gmail.com>
2026-03-12 13:57:43 +0700

    fix migration

--- database/migrations/2025_11_25_125911_category.php
+++ database/migrations/2025_11_25_125911_category.php
@@ -6,24 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description'); // Thêm cột description
+            $table->text('description');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('categories');
     }
 };
